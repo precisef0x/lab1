35f982a9945968c89fc708ba82495c6f6163e1cd
precisef0x <admin@onescript.ru>
2015-02-20 12:13:35 +0300

    Basic functions

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 int main(void)
 {
-
-
-
-return 0;
+	uint32_t N;
+        uint8_t *array;
+	scanf("%u", &N);
+	array = (uint8_t*) malloc(N*8);
+	for(int i=0; i<N; i++) scanf("%u", &array[i]);
+	
+	int diff;
+	
+cycle:;	diff = 0;
+	for(int i=0; i<N-1; i++)
+	{
+		diff=0;
+		if(array[i] > array[i+1])
+		{
+			diff++;
+			unsigned char temp = array[i];
+			array[i] = array[i+1];
+			array[i+1] = temp;
+		}
+	}
+	if(diff) goto cycle;
+	
+	for(int i=0; i<N; i++) printf("%u ", array[i]);
+	
+	getchar();
+	free(array);
+	return 0;
 }
